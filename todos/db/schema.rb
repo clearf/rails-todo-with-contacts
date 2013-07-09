2cf6313cc96335d530def04e62c3df50b341f80f
Christine Coulter <christinetcoulter@gmail.com>
2013-07-09 17:53:49 -0400

    Create migration files for contacts table and joint table (contacts_todos).

--- todos/db/schema.rb
+++ todos/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709034656) do
+ActiveRecord::Schema.define(:version => 20130709215016) do
+
+  create_table "contacts", :force => true do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "contacts_todos", :id => false, :force => true do |t|
+    t.integer "contact_id"
+    t.integer "todo_id"
+  end
 
   create_table "todos", :force => true do |t|
     t.string   "name"
