ffdd62afec2a91c7d57db6588fe411612f4c1007
Wesley Carr <surfswell17@gmail.com>
2013-07-09 21:41:58 -0400

    Finished basic RailsToDo. Adding contacts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,15 +8,42 @@ class TasksController < ApplicationController
   end
 
   def create
+    task = Task.new
+    task.todo = params[:todo]
+    task.details = params[:details]
+    task.due = params[:due]
+    task.status = params[:status]
+
+    task.save
+
+    redirect_to("/tasks/#{task.id}")
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+
+    task.todo = params[:todo]
+    task.details = params[:details]
+    task.due = params[:due]
+    task.status = params[:status]
+
+    task.save
+    redirect_to "/tasks/#{task.id}"
   end
 
   def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    redirect_to("/tasks")
   end
 
 end
