a210791118eee4f79263e3a771b4369c65d0ed8d
Wesley Carr <surfswell17@gmail.com>
2013-07-10 11:00:07 -0400

    Added ruby link_to for navigation & _navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ RailsTodo::Application.routes.draw do
   # Missed this in class... attempting to make it work
   root :to => 'welcome#index'
   resources :tasks, :contacts
+  get '/about' => 'welcome#about'
 
 end
