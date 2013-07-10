5374d16a989313c71a61243cd70322cfae93ece1
Wesley Carr <surfswell17@gmail.com>
2013-07-09 21:58:23 -0400

    Added view pages for contacts. Routing with contacts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RailsTodo::Application.routes.draw do
   # Missed this in class... attempting to make it work
   root :to => 'welcome#index'
-  resources :tasks
+  resources :tasks, :contacts
+
 end
