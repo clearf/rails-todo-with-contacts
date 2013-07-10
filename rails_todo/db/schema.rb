9eedbed486110df8083fc4448eeb281a56013950
Alina Guzman <alinochka8@gmail.com>
2013-07-09 21:46:57 -0400

    Added a join migration for people and tasks table

--- rails_todo/db/schema.rb
+++ rails_todo/db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708232345) do
+ActiveRecord::Schema.define(:version => 20130710013538) do
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "tasks_people", :force => true do |t|
+    t.integer  "task_id"
+    t.integer  "person_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
